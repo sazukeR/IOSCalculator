eec9d734655ee2cf66b17dc87ec5878f6790aebc
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-04-11 11:29:30 -0500

    Add custom hook to build the number of the calculator

---
+++ src/presentation/hooks/useCalculator.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+
+
+export const useCalculator = () => {
+
+
+  const [ number, setNumber ] = useState("0")
+
+
+  const buildNumber = ( numberString: string ) => {
+
+
+    if ( number.includes(".") && numberString === "." ) return;
+
+
+    if ( number.startsWith("0") || number.startsWith("-0") ) {
+
+        // Punto decimal (solo podemos tener un punto decimal en la aplicacion)
+        if ( numberString === "." ) {
+            return setNumber(number + numberString);
+        }
+
+        // Evaluar si es otro cero y no hay punto
+        if ( numberString === "0" && number.includes(".") ) {
+            return setNumber( number + numberString );
+        }
+
+
+        // Evaluar si es diferente de cero, no hay punto, y es el primer mumero
+        if ( numberString !== "0" && !number.includes(".") ) {
+            return setNumber( numberString )
+        }
+
+        // Evitar 000000.00
+        if ( numberString === "0" && !number.includes(".") ) {
+            return;
+        }
+
+        return setNumber( number + numberString );
+    }
+
+    setNumber( number + numberString );
+
+  }
+
+
+
+  return {
+    // Properties
+    number,
+
+
+    // Methods
+    buildNumber,
+
+  }
+  
+}
+
